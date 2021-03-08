34583431411b584d6e03dc6e3e54823e3e4b3dc6
William Arin <williamarin.dev@gmail.com>
2021-03-08 16:43:41 +0800

    feat: new control FormControlDate with date picker

---
+++ src/components/form/controls/FormControlDate.vue
@@ -0,0 +1,47 @@
+<!--
+  - This file is part of the NumberNine package.
+  -
+  - (c) William Arin <williamarin.dev@gmail.com>
+  -
+  - For the full copyright and license information, please view the LICENSE
+  - file that was distributed with this source code.
+  -->
+
+<template>
+    <q-input
+        filled stack-label clearable
+        mask="date"
+        :rules="['date']"
+        :label="parameters.label"
+        :value="value"
+        @input="$emit('input', $event)">
+        <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+                <q-popup-proxy transition-show="scale" transition-hide="scale">
+                    <q-date
+                        :value="value"
+                        @input="$emit('input', $event)"
+                        mask="YYYY-MM-DD"
+                    >
+                        <div class="row items-center justify-end">
+                            <q-btn v-close-popup label="Close" color="primary" flat />
+                        </div>
+                    </q-date>
+                </q-popup-proxy>
+            </q-icon>
+        </template>
+    </q-input>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import FormControlParameters from '../../../model/interfaces/FormControlParameters';
+
+@Component
+export default class FormControlDate extends Vue {
+    @Prop()
+    private value: any;
+
+    @Prop()
+    private parameters: FormControlParameters;
+}
+</script>
